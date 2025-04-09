7cc0e8458607acbed70d90c41d64194b5d2bc545
Yasir Qasemi <yaqa8976@colorado.edu>
2025-04-09 12:52:20 -0600

    deleted some stuff

--- ProjectSourceCode/src/index.js
+++ ProjectSourceCode/src/index.js
@@ -17,9 +17,10 @@ const hbs = handlebars.create({
 });
 
 // Register `hbs` as our view engine using its bound `engine()` function.
-app.engine('hbs', hbs.engine);
+app.engine('hbs', exphbs.engine({ extname: 'hbs', defaultLayout: 'main', layoutsDir: path.join(__dirname, 'src/views') }));
 app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'src/views'));
+
 app.use(bodyParser.json());
 // set Session
 app.use(
@@ -57,91 +58,51 @@ db.connect()
   });
 
 // -------------------------------------  ROUTES for login.hbs   ----------------------------------------------
-const user = {
-  student_id: undefined,
-  username: undefined,
-  first_name: undefined,
-  last_name: undefined,
-  email: undefined,
-  year: undefined,
-  major: undefined,
-  degree: undefined,
+const Users = {
+  user_id : undefined,
+  username : undefined,
+  email : undefined,
+  password_hash,
 };
 
-const student_courses = `
-  SELECT DISTINCT
-    courses.course_id,
-    courses.course_name,
-    courses.credit_hours,
-    students.student_id = $1 AS "taken"
-  FROM
-    courses
-    JOIN student_courses ON courses.course_id = student_courses.course_id
-    JOIN students ON student_courses.student_id = students.student_id
-  WHERE students.student_id = $1
-  ORDER BY courses.course_id ASC;`;
-
-const all_courses = `
-  SELECT
-    courses.course_id,
-    courses.course_name,
-    courses.credit_hours,
-    CASE
-    WHEN
-    courses.course_id IN (
-      SELECT student_courses.course_id
-      FROM student_courses
-      WHERE student_courses.student_id = $1
-    ) THEN TRUE
-    ELSE FALSE
-    END
-    AS "taken"
-  FROM
-    courses
-  ORDER BY courses.course_id ASC;
-  `;
-
-app.get('/login', (req, res) => {
-  res.render('/login');
-});
-app.get('/register', (req, res) => {
-  res.render('/register');
-});
-app.get('/', (req, res) => {
-  res.redirect('/register'); //this will call the /anotherRoute route in the API
-});
-
+  app.get('/', (req, res) => {
+    res.redirect('/register'); 
+  });
+  
+  app.get('/login', (req, res) => {
+    res.render('pages/login');
+  });
+  
+  app.get('/register', (req, res) => {
+    res.render('pages/register');
+  });
 // Login submission
 app.post('/login', (req, res) => {
-  const email = req.body.email;
-  const username = req.body.username;
-  const query = 'select * from students where students.email = $1 LIMIT 1';
+  const { email } = req.body;
+
+  const query = 'SELECT * FROM Users WHERE email = $1 LIMIT 1';
   const values = [email];
 
-  // get the student_id based on the emailid
   db.one(query, values)
     .then(data => {
-      user.student_id = data.student_id;
-      user.username = username;
-      user.first_name = data.first_name;
-      user.last_name = data.last_name;
-      user.email = data.email;
-      user.year = data.year;
-      user.major = data.major;
-      user.degree = data.degree;
+      // Store user info in session
+      req.session.user = {
+        user_id: data.user_id,
+        username: data.username,
+        email: data.email,
+        password: data.password_hash,
+      };
 
-      req.session.user = user;
-      req.session.save();
-
-      res.redirect('/');
+      req.session.save(() => {
+        res.redirect('/');
+      });
     })
     .catch(err => {
-      console.log(err);
+      console.error('Login error:', err);
       res.redirect('/login');
     });
 });
 
-// Authentication middleware.
 const auth = (req, res, next) => {
   if (!req.session.user) {
     return res.redirect('/login');
@@ -149,11 +110,12 @@ const auth = (req, res, next) => {
   next();
 };
 
+
 app.use(auth);
 
 // -------------------------------------  ROUTES for home.hbs   ----------------------------------------------
 
-app.get('/', (req, res) => {
+app.get('/home', (req, res) => {
   res.render('pages/home', {
     username: req.session.user.username,
     first_name: req.session.user.first_name,
