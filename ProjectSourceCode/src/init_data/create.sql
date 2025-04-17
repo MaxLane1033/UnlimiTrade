a5125f29e0c6f33ff5bd70ae518a47a6ae9b2ac9
Yasir Qasemi <yaqa8976@colorado.edu>
2025-04-17 12:14:30 -0600

    changed db

--- ProjectSourceCode/src/init_data/create.sql
+++ ProjectSourceCode/src/init_data/create.sql
@@ -1,12 +1,12 @@
 
-CREATE TABLE Users (
+CREATE TABLE IF NOT EXISTS Users (
     user_id SERIAL PRIMARY KEY,
     username VARCHAR(50) UNIQUE NOT NULL,
     password_hash VARCHAR(255) NOT NULL,
     profile_picture TEXT /*testing this for editing a profile picture*/
 );
 
-CREATE TABLE Items (
+CREATE TABLE IF NOT EXISTS Items (
     item_id SERIAL PRIMARY KEY,
     user_id INT,
     name VARCHAR(100) NOT NULL,
@@ -16,7 +16,7 @@ CREATE TABLE Items (
     FOREIGN KEY (user_id) REFERENCES Users(user_id)
 );
 
-CREATE TABLE TradeBlock (
+CREATE TABLE IF NOT EXISTS TradeBlock (
     trade_id SERIAL PRIMARY KEY,
     proposer_id INT,
     receiver_id INT,
@@ -25,7 +25,7 @@ status VARCHAR(100),
     FOREIGN KEY (receiver_id) REFERENCES Users(user_id)
 );
 
-CREATE TABLE TradeDetails (
+CREATE TABLE IF NOT EXISTS TradeDetails (
     trade_id INT,
     item_id INT,
     shipping_address VARCHAR(100),
@@ -34,7 +34,7 @@ CREATE TABLE TradeDetails (
     FOREIGN KEY (item_id) REFERENCES Items(item_id)
 );
 
-CREATE TABLE Reviews (
+CREATE TABLE IF NOT EXISTS Reviews (
     review_id SERIAL PRIMARY KEY,
     reviewer_id INT,
     reviewed_id INT,
