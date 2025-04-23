f345bd487945340ecc64a3eed2864fe87386155b
ethanadoyle <etdo9322@colorado.edu>
2025-04-23 17:08:51 -0600

    commented out the test user inserts

--- ProjectSourceCode/src/init_data/insert.sql
+++ ProjectSourceCode/src/init_data/insert.sql
@@ -2,13 +2,13 @@
 INSERT INTO Users (username, password_hash, profile_picture)
 VALUES ('ethan', 'password', '/home/ethanadoyle/Soft_dev/UnlimiTrade/ProjectSourceCode/public/uploads/ethanprofile.png');
 
--- Insert test users
-INSERT INTO Users (username, password_hash, profile_picture)
-VALUES ('testuser2', 'password', '/uploads/user2.png');
+-- -- Insert test users
+-- INSERT INTO Users (username, password_hash, profile_picture)
+-- VALUES ('testuser2', 'password', '/uploads/user2.png');
 
--- Insert items for that user (use subquery to get correct ID)
-INSERT INTO Items (user_id, name, description, category, status, image_path)
-VALUES 
-  ((SELECT user_id FROM Users WHERE username = 'testuser2'), 'Vintage Clock', 'Antique clock.','test', 'available', '/uploads/clock.jpg');
- -- ((SELECT user_id FROM Users WHERE username = 'testuser2'), 'Bluetooth Speaker', 'Great sound.', 'available', '/uploads/speaker.jpg');
+-- -- Insert items for that user (use subquery to get correct ID)
+-- INSERT INTO Items (user_id, name, description, category, status, image_path)
+-- VALUES 
+--   ((SELECT user_id FROM Users WHERE username = 'testuser2'), 'Vintage Clock', 'Antique clock.','test', 'available', '/uploads/clock.jpg');
+--  -- ((SELECT user_id FROM Users WHERE username = 'testuser2'), 'Bluetooth Speaker', 'Great sound.', 'available', '/uploads/speaker.jpg');
 
